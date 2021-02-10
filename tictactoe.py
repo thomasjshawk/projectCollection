5a173de522fa6309f2739e9476def4c1b93fe225
thomasjshawk <thomas.j.shawk@gmail.com>
2021-02-10 14:57:57 -0500

    building player choice of marker + replay option

--- tictactoe.py
+++ tictactoe.py
@@ -2,45 +2,65 @@ import random
 from calculator import replay
 
 
-startingboard = {1 : " ", 2 : " ", 3 : " ", 4 : " ", 5 : " ", 6 : " ", 7 : " ", 8 : " ", 9 : " "}
+starting_board = 'none'
+marker_player = 'none'
+marker_computer = 'none'
+board = 'none'
+
+def markerSelection():
+    global marker_player
+    global marker_computer
+    marker_player = input("Would you like Xs or Os?")
+
+    if marker_player == 'X' or 'X' == 'x':
+        marker_player = 'X'
+        marker_computer = 'O'
+
+    elif marker_player == 'O' or marker_player == 'o':
+        marker_player = 'O'
+        marker_computer = 'X'
 
 def printboard():
     global board 
-    board = f" {startingboard[1]} | {startingboard[2]} | {startingboard[3]} \n___|___|___\n   |   |   \n {startingboard[4]} | {startingboard[5]} | {startingboard[6]} \n___|___|___\n   |   |   \n {startingboard[7]} | {startingboard[8]} | {startingboard[9]} "
+    board = f" {starting_board[1]} | {starting_board[2]} | {starting_board[3]} \n___|___|___\n   |   |   \n {starting_board[4]} | {starting_board[5]} | {starting_board[6]} \n___|___|___\n   |   |   \n {starting_board[7]} | {starting_board[8]} | {starting_board[9]} "
     print(board)
 
 def playerselect():
     playerchoice = int(input("What square would you like to choose? (1-9, left to right, top to bottom)"))
-    if startingboard[playerchoice] != " ":
+    if starting_board[playerchoice] != " ":
         print("Square already taken, choose again!")
         playerselect()
     else:
-        startingboard[playerchoice] = "X"
+        starting_board[playerchoice] = marker_player
 
 
 def computerselect():
     computerchoice = random.randint(1, 9)
-    if startingboard[computerchoice] != " ":
+    if starting_board[computerchoice] != " ":
         computerselect()
     else:
-        startingboard[computerchoice] = 'O'
+        starting_board[computerchoice] = marker_computer
 
-def main():
+
+def game():
+    global starting_board
+    starting_board = {1 : " ", 2 : " ", 3 : " ", 4 : " ", 5 : " ", 6 : " ", 7 : " ", 8 : " ", 9 : " "}
     gameover = False
+    markerSelection()
     while not gameover:
-        if startingboard[1] == startingboard[2] == startingboard[3] == 'X' or startingboard[4] == startingboard[5] == startingboard[6] == 'X' or startingboard[7] == startingboard[8] == startingboard[9] == 'X' or startingboard[1] == startingboard[5] == startingboard[9] == 'X' or startingboard[3] == startingboard[5] == startingboard[7] == 'X' or startingboard[1] == startingboard[4] == startingboard[7] == 'X' or startingboard[2] == startingboard[5] == startingboard[8] == 'X' or startingboard[3] == startingboard[6] == startingboard[9] == 'X':
+        if starting_board[1] == starting_board[2] == starting_board[3] == marker_player or starting_board[4] == starting_board[5] == starting_board[6] == marker_player or starting_board[7] == starting_board[8] == starting_board[9] == marker_player or starting_board[1] == starting_board[5] == starting_board[9] == marker_player or starting_board[3] == starting_board[5] == starting_board[7] == marker_player or starting_board[1] == starting_board[4] == starting_board[7] == marker_player or starting_board[2] == starting_board[5] == starting_board[8] == marker_player or starting_board[3] == starting_board[6] == starting_board[9] == marker_player:
             print("\n\n")
             print(board)
             print("You win!!")
             gameover = True
             
-        elif startingboard[1] == startingboard[2] == startingboard[3] == 'O' or startingboard[4] == startingboard[5] == startingboard[6] == 'O' or startingboard[7] == startingboard[8] == startingboard[9] == 'O' or startingboard[1] == startingboard[5] == startingboard[9] == 'O' or startingboard[3] == startingboard[5] == startingboard[7] == 'O' or startingboard[1] == startingboard[4] == startingboard[7] == 'O' or startingboard[2] == startingboard[5] == startingboard[8] == 'O' or startingboard[3] == startingboard[6] == startingboard[9] == 'O':
+        elif starting_board[1] == starting_board[2] == starting_board[3] == marker_computer or starting_board[4] == starting_board[5] == starting_board[6] == marker_computer or starting_board[7] == starting_board[8] == starting_board[9] == marker_computer or starting_board[1] == starting_board[5] == starting_board[9] == marker_computer or starting_board[3] == starting_board[5] == starting_board[7] == marker_computer or starting_board[1] == starting_board[4] == starting_board[7] == marker_computer or starting_board[2] == starting_board[5] == starting_board[8] == marker_computer or starting_board[3] == starting_board[6] == starting_board[9] == marker_computer:
             print("\n\n")
             print(board)
             print("You lose!!")
             gameover = True
 
-        elif startingboard[1] != " " and startingboard[2] != " " and startingboard[3] != " " and startingboard[4] != " " and startingboard[5] != " " and startingboard[6] != " " and startingboard[7] != " " and startingboard[8] != " " and startingboard[9] != " ":
+        elif starting_board[1] != " " and starting_board[2] != " " and starting_board[3] != " " and starting_board[4] != " " and starting_board[5] != " " and starting_board[6] != " " and starting_board[7] != " " and starting_board[8] != " " and starting_board[9] != " ":
             print("\n\n")
             print(board)
             print("It's a tie!!")
@@ -50,5 +70,9 @@ def main():
             playerselect()
             computerselect()
             printboard()
+
+
+def main():
+    replay(game)
 
 main()
